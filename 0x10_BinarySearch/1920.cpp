0c17a4bae7c4bd39e58b2512533067f255108abd
AshtonSW <sprauncy76@gmail.com>
2023-08-08 06:47:42 +0900

    Update solved problem(Binary Search)

---
+++ 0x10_BinarySearch/1920.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[100005];
+int n, m;
+
+int binarysearch(int target){
+    int st = 0;
+    int en = n-1;
+    while(st <= en){
+        int mid = (st+en)/2;
+        if(a[mid] < target) st = mid+1;
+        else if (a[mid] > target) en = mid-1;
+        else return 1;
+    }
+    return 0;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a+n);
+    cin >> m;
+    while(m--){
+        int t;
+        cin >> t;
+        cout << binarysearch(t) << '\n';
+    }
+}
