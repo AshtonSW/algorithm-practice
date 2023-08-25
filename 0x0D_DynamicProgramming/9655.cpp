3b09919c2139a96b03bae75124cceebcc7cb9cd3
AshtonSW <sprauncy76@gmail.com>
2023-08-25 23:46:53 +0900

    update solved problem(DP)

---
+++ 0x0D_DynamicProgramming/9655.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "CY";
+	}
+	else cout << "SK";
+}
