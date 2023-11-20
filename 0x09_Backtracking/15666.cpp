7c9726ee81c86887f8b601aaa4da5441b2d53cce
AshtonSW <sprauncy76@gmail.com>
2023-11-20 23:57:14 +0900

    solved problem

---
+++ 0x09_Backtracking/15666.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[10];
+int num[10];
+int n, m;
+
+void func(int k, int st) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+	int tmp = -1;
+	for (int i = st; i < n; i++) {
+		if (num[i] != tmp) {
+			arr[k] = num[i];
+			tmp = arr[k];
+			func(k + 1, i);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+		
+	}
+	sort(num, num + n);
+	func(0, 0);
+	
+}
